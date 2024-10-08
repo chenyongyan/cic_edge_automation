96d9b2ad840c5f18bea05d3115b97694ed456601
Administrator <chenyongyan1525@gmail.com>
2024-10-08 19:55:50 +0800

    update

--- testcase/testCases.py
+++ testcase/testCases.py
@@ -14,17 +14,12 @@ class case(unittest.TestCase):
         self.util = util()
         ConfPath = self.readConfig.readConfig("Path", "conf_dir")
         File = open(file=ConfPath + "config.json", mode='r', encoding='UTF-8')
-        self.file = json.load(File)
+        self.jsondata = json.load(File)
         self.key_list = []
 
 
     def test_get_values_01(self):
-        """
-        返回数据表格值
-        :param :
-        :return:
-        """
-        for root , dirs, files in os.walk(self.readConfig.readConfi("Path","file_dir")):
+        for root , dirs, files in os.walk(self.readConfig.readConfig("Path","file_dir")):
             for name in files:
                 if name.endswith(".xlsx"):
                     pass
@@ -34,38 +29,38 @@ class case(unittest.TestCase):
                     pass
                 else:
                     os.remove(os.path.join(root, name))
-        for root, dirs, files in os.walk(self.readConfig.readConfi("Path","log_dir")):
+        for root, dirs, files in os.walk(self.readConfig.readConfig("Path","log_dir")):
             for name in files:
                 os.remove(os.path.join(root, name))
-        for root, dirs, files in os.walk(self.readConfig.readConfi("Path","report_dir")):
+        for root, dirs, files in os.walk(self.readConfig.readConfig("Path","report_dir")):
             for name in files:
                 os.remove(os.path.join(root, name))
 
-        test_file_list =  os.listdir(self.readConfig.readConfi("Path","file_dir"))
+        test_file_list =  os.listdir(self.readConfig.readConfig("Path","file_dir"))
         for fileName in test_file_list:
             os.path.join('{}{}'.format(test_file_list, fileName))
-            wb = openpyxl.load_workbook(self.readConfig.readConfi("Path","file_dir") + fileName)
+            wb = openpyxl.load_workbook(self.readConfig.readConfig("Path","file_dir") + fileName)
             sheet_names_list = wb.sheetnames
             for sheet_name in sheet_names_list:
                 if sheet_name != "配置文件":
-                    pd = pandas.read_excel(io=self.readConfig.readConfi("Path","file_dir") + fileName, sheet_name=sheet_name, keep_default_na=False)
+                    pd = pandas.read_excel(io=self.readConfig.readConfig("Path","file_dir") + fileName, sheet_name=sheet_name, keep_default_na=False)
                     for rows in pd.index.values:
                         d = dict()
                         # 用例编号
                         d['key1'] = pd.iloc[rows, 0]
-                        ID = str(d['key1']).replace("\n","").replace(" ","").replace("，",",")
+                        test_id = str(d['key1']).replace("\n","").replace(" ","").replace("，",",")
 
                         # 测试类型
                         d['key2'] = pd.iloc[rows, 1]
-                        Subject = str(d['key2']).replace("\n","").replace(" ","").replace("，",",")
+                        test_type = str(d['key2']).replace("\n","").replace(" ","").replace("，",",")
 
                         # 测试点
                         d['key3'] = pd.iloc[rows, 2]
-                        Point = str(d['key3']).replace("\n", "").replace(" ","").replace("，",",")
+                        test_point = str(d['key3']).replace("\n", "").replace(" ","").replace("，",",")
 
                         # 用例标题
                         d['key4'] = pd.iloc[rows, 3]
-                        Title = str(d['key4']).replace("\n", "").replace(" ","").replace("，",",")
+                        test_title = str(d['key4']).replace("\n", "").replace(" ","").replace("，",",")
 
                         # 设备MID
                         list_mid = []
@@ -73,7 +68,7 @@ class case(unittest.TestCase):
                         textTestDevice = str(d['key5']).replace("\n", "").replace(" ","").replace("，",",")
                         list_mid.append(textTestDevice)
                         str_mid = str(list_mid).replace("['","{").replace("']","}")
-                        TestDevice = eval(str(str_mid))
+                        test_device = eval(str(str_mid))
 
                         # 设备条件
                         list_deviceSetUp = []
@@ -81,7 +76,7 @@ class case(unittest.TestCase):
                         textdeviceSetUp = str(d['key6']).replace("\n", "").replace(" ","").replace("，",",")
                         list_deviceSetUp.append(textdeviceSetUp)
                         str_deviceSetUp = str(list_deviceSetUp).replace("['", "{").replace("']", "}")
-                        deviceSetUp = eval(str(str_deviceSetUp))
+                        test_device_setup = eval(str(str_deviceSetUp))
 
                         # 网关条件
                         list_gatwaySetUp = []
@@ -89,7 +84,7 @@ class case(unittest.TestCase):
                         textgatwaySetUp = str(d['key7']).replace("\n", "").replace(" ","").replace("，",",")
                         list_gatwaySetUp.append(textgatwaySetUp)
                         str_gatwaySetUp = str(list_gatwaySetUp).replace("['", "{").replace("']", "}")
-                        gatwaySetUp = eval(str(str_gatwaySetUp))
+                        test_gatway_setup = eval(str(str_gatwaySetUp))
 
                         # 操作步骤
                         list_Setpe = []
@@ -97,7 +92,7 @@ class case(unittest.TestCase):
                         textSetpe = str(d['key8']).replace("\n","").replace(" ","").replace("，",",")
                         list_Setpe.append(textSetpe)
                         str_Setpe = str(list_Setpe).replace("['", "{").replace("']", "}")
-                        Setpe = eval(str(str_Setpe))
+                        test_setpe = eval(str(str_Setpe))
 
                         # 期望输出队列信息
                         list_Queue = []
@@ -105,30 +100,29 @@ class case(unittest.TestCase):
                         textQueue = str(d['key9']).replace("\n", "").replace(" ","").replace("，",",")
                         list_Queue.append(textQueue)
                         str_Queue = str(list_Queue).replace("['", "{").replace("']", "}")
-                        Queue = eval(str(str_Queue))
-                        self.key_list.append([rows,ID,Subject,Point,Title,TestDevice,deviceSetUp,gatwaySetUp,Setpe,Queue])
-
+                        test_expect_queue = eval(str(str_Queue))
+                        self.key_list.append([rows, test_id, test_type, test_point, test_title, test_device, test_device_setup, test_gatway_setup, test_setpe, test_expect_queue])
         return self.key_list
 
 
     @data(*test_get_values_01())
     @unpack
-    def test_start_02(self, ID, Title, deviceSetUp, gatwaySetUp, Setpe, Queue):
+    def test_start_02(self, test_id, test_title, test_device_setup, test_gatway_setup, test_setpe, test_expect_queue):
 
         # 网关智控初始化
-        self.util.test_setup(caseTitle=Title)
+        self.util.test_setup(caseTitle=test_title)
 
         # 设备条件初始化
-        self.util.device_setup(deviceStatus=deviceSetUp)
+        self.util.device_setup(deviceStatus=test_device_setup)
 
         # 网关条件初始化
-        self.util.gatway_setup(gatwayStatus=gatwaySetUp)
+        self.util.gatway_setup(gatwayStatus=test_gatway_setup)
 
         # 用例执行操作步骤
-        self.util.test_setpe(Setpe=Setpe, Title=Title)
+        self.util.test_setpe(Setpe=test_setpe, Title=test_title)
 
         # 用例断言
-        self.util.get_gatway_queue_assert(test_id=ID, queue_info=Queue)
+        self.util.get_gatway_queue_assert(test_id=test_id, queue_info=test_expect_queue)
 
 
 if __name__=="__main__":
